c18c86b47e8e2b570246297fcf48f660fbad46e3
jrwang8 <47158965+jrwang8@users.noreply.github.com>
2019-08-13 23:08:54 -0400

    Add the Comments

--- merchants/src/main/java/com/imooc/passbook/merchants/service/IMerchantsServ.java
+++ merchants/src/main/java/com/imooc/passbook/merchants/service/IMerchantsServ.java
@@ -7,25 +7,26 @@ import com.imooc.passbook.merchants.vo.Response;
 
 /**
  * 对商户服务接口定义
+ * The Merhcants Service Interface
  */
 public interface IMerchantsServ {
 
     /**
-     * 创建商户服务
+     * 创建商户服务 Creat the Merchants Service
      * @param request {@link CreateMerchantsRequest} 创建商户请求
      * @return {@link Response}
      */
     Response createMerchants(CreateMerchantsRequest request);
 
     /**
-     * 根据Id构造商户信息
+     * 根据Id构查询商户信息 Query the Merchants Info by Id
      * @param id 商户id
      * @return {@link Response}
      */
     Response buildMerchantsInfoById(Integer id);
 
     /**
-     * 投放优惠券
+     * 投放优惠券 Drop the Coupon Service
      * @param template {@link PassTemplate} 优惠券对象
      * @return {@link Response}
      */
