92c137b821c37d5bacaa3a58563eec18f6f38f5e
jrwang8 <47158965+jrwang8@users.noreply.github.com>
2019-08-13 22:46:42 -0400

    Add the comments

--- merchants/src/main/java/com/imooc/passbook/merchants/constant/Constants.java
+++ merchants/src/main/java/com/imooc/passbook/merchants/constant/Constants.java
@@ -1,25 +1,24 @@
 package com.imooc.passbook.merchants.constant;
 
 /**
- * 普通(通用)常量定义
- * 如果对常量定义不是区分的很严格但很常用，就可以把常量定义在这里
+ * Constants Definition
  */
 public class Constants {
 
     /**
-     * 商户优惠券投放的Kafka Topic
+     * Kafka Topic of Merchants Dropping Coupon
      */
     public static final String TEMPLATE_TOPIC = "merchants-template";
 
     /**
-     * 商户通用的token，在HTTP request的header里是key-value存在的
-     * 这里的token string就是key
+     * Coupon Token, the form of which is key-value pair in HTTP Header
+     * This TOEKN_STRING is the key
      */
     public static final String TOKEN_STRING = "token";
 
     /**
-     * 实际颁发给商户进行使用的token info，用来通过平台验证
-     * 这里的token就是value
+     * The token info to give to the Merchants to pass the validation
+     * This is the value
      */
     public static final String TOKEN = "imooc-passbook-merchants";
 }
