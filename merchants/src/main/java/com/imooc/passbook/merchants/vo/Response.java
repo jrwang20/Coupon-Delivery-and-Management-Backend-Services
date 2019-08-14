c606eb9945d3f10bc92e4cc486d542afda5f602e
jrwang8 <47158965+jrwang8@users.noreply.github.com>
2019-08-13 23:22:02 -0400

    Update Response.java

--- merchants/src/main/java/com/imooc/passbook/merchants/vo/Response.java
+++ merchants/src/main/java/com/imooc/passbook/merchants/vo/Response.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * 通用响应对象
+ * 通用响应对象 the Response VO to store all the response data
  * 在HTTP响应时，需要有一个同一个对象进行承载封装，才能传给客户端
  * 所有HTTP response都是这个
  */
@@ -16,16 +16,19 @@ public class Response {
 
     /**
      * 错误码，正确返回0
+     * Error Code, return 0 if success
      */
     private Integer errorCode = 0;
 
     /**
      * 错误信息，正确返回空字符串
+     * Error Message, return empty if success
      */
     private String errorMsg = "";
 
     /**
      * 返回值对象
+     * Response Data Object, return null if error
      */
     private Object data;
 
